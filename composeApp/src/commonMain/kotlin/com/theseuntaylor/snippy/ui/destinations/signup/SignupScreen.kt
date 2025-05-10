f0d8748c5a2941ea47f8b41f5ddc615ac90e1d12
oluwaseun <seun.gtaylor@gmail.com>
2025-05-11 00:59:31 +0100

    refactor: Refactor signup screen UI and add input validation strings
    This commit refactors the UI of the signup screen to use shared `TextInput` and `PasswordTextInputField` composables. It also introduces new string resources for input validation messages.
    
    **Key Changes:**
    
    -   **UI Refactoring:**
        -   Replaced individual `OutlinedTextField` usages with the shared `TextInput` and `PasswordTextInputField` components.
        -   Modified the `SignupScreen` composable to manage input states and pass them to a new `SignupScreenContent` composable for cleaner separation of concerns.
        -   Added `passwordState` and `confirmPasswordState` using `rememberTextFieldState` for password input management.
    -   **String Resources:**
        -   Added `valid_first_name` and `valid_last_name` strings to `strings.xml` for input validation error messages.
    -   **Minor Changes:**
        -   Removed unused imports and updated placeholder text for email input.

--- composeApp/src/commonMain/kotlin/com/theseuntaylor/snippy/ui/destinations/signup/SignupScreen.kt
+++ composeApp/src/commonMain/kotlin/com/theseuntaylor/snippy/ui/destinations/signup/SignupScreen.kt
@@ -2,17 +2,13 @@ package com.theseuntaylor.snippy.ui.destinations.signup
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material.icons.outlined.Warning
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,181 +17,133 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.theseuntaylor.snippy.ui.components.SnippyButton
+import com.theseuntaylor.snippy.ui.destinations.shared.PasswordTextInputField
+import com.theseuntaylor.snippy.ui.destinations.shared.TextInput
+import com.theseuntaylor.snippy.ui.model.InputFieldError
+import com.theseuntaylor.snippy.ui.destinations.shared.TextInputField
 import org.jetbrains.compose.resources.stringResource
 import snippy.composeapp.generated.resources.Res
-import snippy.composeapp.generated.resources.confirm_password
 import snippy.composeapp.generated.resources.create_account
-import snippy.composeapp.generated.resources.email
+import snippy.composeapp.generated.resources.email_address
 import snippy.composeapp.generated.resources.first_name
 import snippy.composeapp.generated.resources.last_name
 import snippy.composeapp.generated.resources.password
-import snippy.composeapp.generated.resources.url_hint
+import snippy.composeapp.generated.resources.valid_first_name
+import snippy.composeapp.generated.resources.valid_last_name
+
 
 @Composable
-fun SignupScreen(
-    modifier: Modifier = Modifier,
-) {
-
+fun SignupScreen(modifier: Modifier = Modifier) {
     var firstNameInput by remember { mutableStateOf("") }
     var lastNameInput by remember { mutableStateOf("") }
     var emailInput by remember { mutableStateOf("") }
-    var passwordInput by remember { mutableStateOf("") }
-    var confirmPasswordInput by remember { mutableStateOf("") }
-
-    var passwordVisibility by remember { mutableStateOf(false) }
-    var confirmPasswordVisibility by remember { mutableStateOf(false) }
+    val passwordState = rememberTextFieldState()
+    val confirmPasswordState = rememberTextFieldState()
+    val passwordError by remember { mutableStateOf(InputFieldError.Initial) }
+    val isButtonEnabled by remember { mutableStateOf(false) }
 
     Column(
-        modifier = modifier.padding(
+        modifier = Modifier.padding(
             horizontal = 20.dp,
             vertical = 40.dp,
         ).fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(20.dp),
     ) {
-        Text(
-            stringResource(Res.string.create_account),
-            style = MaterialTheme.typography.titleLarge,
-            fontWeight = FontWeight.Bold,
-        )
-
-        Column {
-            Text("First Name")
-            OutlinedTextField(
-                value = firstNameInput,
-                onValueChange = { firstNameInput = it },
-                placeholder = {
-                    Text(
-                        text = stringResource(resource = Res.string.first_name),
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next
-                )
-            )
-        }
-
-        Column {
-            Text("Last Name")
-            OutlinedTextField(
-                value = lastNameInput,
-                onValueChange = { lastNameInput = it },
-                placeholder = {
-                    Text(
-                        text = stringResource(resource = Res.string.last_name),
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next
-                )
-            )
-        }
-
-        Column {
-            Text("Email Address")
-            OutlinedTextField(
-                value = emailInput,
-                onValueChange = { emailInput = it },
-                placeholder = {
-                    Text(
-                        text = stringResource(resource = Res.string.email),
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next
-                )
-            )
-        }
-
-        Column {
-            Text("Password")
-            OutlinedTextField(
-                value = passwordInput,
-                onValueChange = { passwordInput = it },
-                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-                placeholder = {
-                    Text(
-                        text = stringResource(resource = Res.string.password),
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        passwordVisibility = !passwordVisibility
-                    }) {
-                        if (passwordVisibility) {
-                            Icon(
-                                Icons.Filled.Warning,
-                                contentDescription = "Toggle password visibility"
-                            )
-                        } else {
-                            Icon(
-                                Icons.Filled.Warning,
-                                contentDescription = "Toggle password visibility"
-                            )
-                        }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Next
-                )
-            )
-        }
-
-        Column {
-            Text("Confirm Password")
-            OutlinedTextField(
-                value = confirmPasswordInput,
-                onValueChange = { confirmPasswordInput = it },
-                visualTransformation = if (confirmPasswordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-                placeholder = {
-                    Text(
-                        text = stringResource(resource = Res.string.confirm_password),
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        confirmPasswordVisibility = !confirmPasswordVisibility
-                    }) {
-                        if (passwordVisibility) {
-                            Icon(
-                                Icons.Outlined.Warning,
-                                contentDescription = "Toggle password visibility"
-                            )
-                        } else {
-                            Icon(
-                                Icons.Filled.Warning,
-                                contentDescription = "Toggle password visibility"
-                            )
-                        }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                )
-            )
-        }
-        SnippyButton(
-            onClick = { /*TODO*/ },
-            modifier = Modifier.fillMaxWidth(),
-            textContent = stringResource(resource = Res.string.create_account),
-            containerColor = MaterialTheme.colorScheme.primary,
+        SignupScreenContent(
+            firstNameInput = firstNameInput,
+            lastNameInput = lastNameInput,
+            emailInput = emailInput,
+            passwordState = passwordState,
+            confirmPasswordState = confirmPasswordState,
+            passwordError = passwordError,
+            onEmailChange = { emailInput = it },
+            onFirstNameChange = { firstNameInput = it },
+            onLastNameChange = { lastNameInput = it },
+            isButtonEnabled = isButtonEnabled
         )
     }
+
+}
+
+@Composable
+fun ColumnScope.SignupScreenContent(
+    firstNameInput: String,
+    lastNameInput: String,
+    emailInput: String,
+    passwordState: TextFieldState,
+    confirmPasswordState: TextFieldState,
+    passwordError: InputFieldError,
+    isButtonEnabled: Boolean,
+    onEmailChange: (String) -> Unit,
+    onFirstNameChange: (String) -> Unit,
+    onLastNameChange: (String) -> Unit,
+) {
+
+    Text(
+        stringResource(Res.string.create_account),
+        style = MaterialTheme.typography.titleLarge,
+        fontWeight = FontWeight.Bold,
+    )
+
+    // could mod this to have ime actions for keyboard.
+    TextInput(
+        sectionTitle = stringResource(resource = Res.string.first_name),
+        showErrorMessage = true,
+        errorMessage = stringResource(Res.string.valid_first_name),
+        inputField = {
+            TextInputField(
+                value = firstNameInput,
+                sectionTitle = stringResource(Res.string.first_name),
+                onValueChange = onFirstNameChange,
+            )
+        })
+
+    TextInput(
+        sectionTitle = stringResource(resource = Res.string.last_name),
+        showErrorMessage = true,
+        errorMessage = stringResource(Res.string.valid_last_name),
+        inputField = {
+            TextInputField(
+                value = lastNameInput,
+                sectionTitle = stringResource(Res.string.last_name),
+                onValueChange = onLastNameChange,
+            )
+        })
+
+    TextInput(
+        sectionTitle = stringResource(resource = Res.string.email_address),
+        showErrorMessage = true,
+        errorMessage = "Enter a valid email address",
+        inputField = {
+            TextInputField(
+                value = emailInput,
+                sectionTitle = stringResource(resource = Res.string.email_address),
+                onValueChange = onEmailChange,
+            )
+        },
+    )
+    TextInput(
+        sectionTitle = stringResource(resource = Res.string.password),
+        showErrorMessage = passwordError.shouldShowError,
+        errorMessage = "Enter at least 8 characters",
+        modifier = Modifier.padding(vertical = 20.dp),
+        inputField = { PasswordTextInputField(passwordState = passwordState) },
+    )
+    TextInput(
+        sectionTitle = stringResource(resource = Res.string.password),
+        showErrorMessage = passwordError.shouldShowError,
+        errorMessage = "Enter at least 8 characters",
+        modifier = Modifier.padding(vertical = 20.dp),
+        inputField = { PasswordTextInputField(passwordState = confirmPasswordState) },
+    )
+
+    SnippyButton(
+        onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth(),
+        enabled = isButtonEnabled,
+        textContent = stringResource(resource = Res.string.create_account),
+        containerColor = MaterialTheme.colorScheme.primary,
+    )
 }
